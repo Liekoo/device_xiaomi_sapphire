b603dd756a59443fe8904ca28152a92643118fe6
lieko <lycoreyes099@gmsil.com>
2025-03-17 06:09:20 +0000

    Pixel OS Bringup

--- aosp_sapphire.mk
+++ aosp_sapphire.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sapphire/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device configs
 TARGET_BOOT_ANIMATION_RES = 1080
@@ -26,7 +26,7 @@ BUILD_BCR := false
 TARGET_INCLUDE_ACCORD := false
 EVO_BUILD_TYPE := Unofficial
 
-PRODUCT_NAME := lineage_sapphire
+PRODUCT_NAME := aosp_sapphire
 PRODUCT_DEVICE := sapphire
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
